75eb6ce8c7cf7f445d1fc3a5402a8fe158b4da6e
Tiziozero2 <kleidiv123@gmail.com>
2026-05-15 23:35:22 +0100

    expr binops

--- src/symbols.rs
+++ src/symbols.rs
@@ -10,7 +10,7 @@ pub struct TypeId(pub usize);
 #[derive(Debug,Clone)]
 pub struct Object {
     name: String,
-    ty: Option<Type>,
+    ty: Option<TypeId>,
 }
 #[derive(Debug,Clone,PartialEq)]
 pub struct FnArg {
@@ -111,13 +111,25 @@ impl<'ctx> SymbolResolver<'ctx> {
         }
     }
     fn resolve_expr(&mut self, exprid: ExprId) -> Result<(), String> {
-        let p = self.get_current_ast()?;
-        match p.get_expr(exprid).unwrap() {
+        let expr = self.get_current_ast()?.get_expr(exprid).unwrap().clone();
+        match expr {
+            parser::Expr::Number(_) => Ok(()),
+            parser::Expr::Symbol(s) => {
+                let name = s.symbol.clone();
+                let id = self.scope_get_object(&name)?;
+                self.refs.insert(exprid, id);
+                Ok(())
+            },
+            parser::Expr::Binop(binop) => {
+                self.resolve_expr(binop.left)?;
+                self.resolve_expr(binop.right)?;
+                Ok(())
+            }
             e => panic!("Impl expr check for {:?}", e),
         }
         // Ok(())
     }
-    fn new_object(&mut self, name: String, ty: Option<Type>) -> Result<ObjectId, String> {
+    fn new_object(&mut self, name: String, ty: Option<TypeId>) -> Result<ObjectId, String> {
         let o = Object { name: name.clone(), ty };
         let id = self.ctx.new_object(o);
         self.get_scope_mut(self.current_scope).unwrap().new_object(name, id);
@@ -134,7 +146,9 @@ impl<'ctx> SymbolResolver<'ctx> {
                             match s.parent {
                                 Some(pid) => id = pid,
                                 None =>
-                                    return Err(String::from("Type does not exist"))
+                                    return Err(String::from(
+                                            format!("object {} does not exist.",
+                                                name)))
                             }
                         }
                     }
@@ -203,8 +217,10 @@ impl<'ctx> SymbolResolver<'ctx> {
             args: argdecs,
             ret_ty,
         });
+        let interned = self.ctx.intern_type(fn_ty.clone());
         // define self for recursion
-        self.new_object(fn_dec.name.clone(), Some(fn_ty.clone()))?;
+        self.new_object(fn_dec.name.clone(),
+                Some(interned))?;
         // make sure body's alright
         if let Some(b) = fn_dec.body {
             self.resolve_fndec_body(b)?;
@@ -212,7 +228,10 @@ impl<'ctx> SymbolResolver<'ctx> {
             panic!("Fn must have body");
         }
         self.exit_scope();
-        self.new_object(fn_dec.name.clone(), Some(fn_ty.clone()))?;
+        let interned = self.ctx.intern_type(fn_ty.clone());
+        // define it in scope
+        self.new_object(fn_dec.name.clone(),
+                Some(interned))?;
         Ok(())
     }
     fn resolve_item(&mut self, itemid: ItemId) -> Result<(), String> {
@@ -234,6 +253,22 @@ impl<'ctx> SymbolResolver<'ctx> {
         }
         Ok(())
     }
+    fn resolve_vardec(&mut self, vardec: &parser::VarDec)  -> Result<(), String> {
+        let t = if let Some(ty) = vardec.ty.clone() {
+            let clone = ty.clone();
+            let r = self.resolve_type(&clone)?; 
+            Some(r)
+        } else {
+            None
+        };
+        let name = vardec.s.clone();
+        if let Some(v) = vardec.val {
+            debugln!("verdec has expression");
+            self.resolve_expr(v)?;
+        }
+        self.new_object(name, t)?; // create object
+        Ok(())
+    }
     fn resolve_stmt(&mut self, stmtid: StmtId) -> Result<(), String> {
         let stmt = self.current_p.as_mut().unwrap().get_stmt(stmtid).unwrap();
         match stmt.clone() {
@@ -241,18 +276,7 @@ impl<'ctx> SymbolResolver<'ctx> {
                 self.resolve_expr(exprid)
             },
             parser::Stmt::VarDec(vardec) => {
-                let t = if let Some(ty) = vardec.ty.clone() {
-                    let clone = ty.clone();
-                    let r = self.resolve_type(&clone); 
-                    Some(r)
-                } else {
-                    None
-                };
-                let name = vardec.s.clone();
-                if let Some(v) = vardec.val {
-                    self.resolve_expr(v)?;
-                }
-                panic!("impl");
+                self.resolve_vardec(&vardec)
             },
             s => panic!("Impl stmt check for {:?}", s),
         }
@@ -261,8 +285,9 @@ impl<'ctx> SymbolResolver<'ctx> {
         self.resolve_mod_decs(modid)?;
         Ok(())
     }
-    fn get_current_ast(&mut self) -> Result<&parser::Parser,String> {
-        let p = self.current_p.as_ref().ok_or(String::from("No cuurent parser in st"))?;
+    fn get_current_ast(&mut self) -> Result<&mut parser::Parser,String> {
+        let p = self.current_p.as_mut()
+            .ok_or(String::from("No cuurent parser in st"))?;
         Ok(p)
     }
     pub fn resolve(&mut self, p: parser::Parser) -> Result<(), String> {
